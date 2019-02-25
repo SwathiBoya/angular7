e614be68be2eb56bd873ca450ddccdeac19d06ec
SwathiBoya <boyaswathi25@gmail.com>
2019-02-25 11:36:30 +0530

    Favicon changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
+declare function myMethod(): any;
 @Component({
   selector: 'swathi-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular7';
+  ngOnInit(){
+    myMethod();
+  }
 }
